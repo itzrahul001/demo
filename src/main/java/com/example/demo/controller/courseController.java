2dfdf0ca139bc62e062908ecbb947fcfe8609543
Rahul Yadav <rahulyadav96962004@gmail.com>
2026-02-11 19:23:03 +0530

    api change

--- src/main/java/com/example/demo/controller/courseController.java
+++ src/main/java/com/example/demo/controller/courseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/courses")
 @CrossOrigin(origins = "*")
 public class courseController {
 
